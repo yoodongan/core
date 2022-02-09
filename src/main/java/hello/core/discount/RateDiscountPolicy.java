7e181b7ac9ec6d0beaf3aa03e55c3b643b14a295
yoodongan <dbehddks519@naver.com>
2022-02-09 14:34:28 +0900

    fix : implements 빼먹음.

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -3,8 +3,8 @@ package hello.core.discount;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 
-public class RateDiscountPolicy {
-    private int discount  = 10; // 할인율 10%를 의미.
+public class RateDiscountPolicy implements DiscountPolicy{
+    private final int discount  = 10; // 할인율 10%를 의미.
 
     public int discount(Member member, int price){
         if (member.getGrade() == Grade.VIP){
