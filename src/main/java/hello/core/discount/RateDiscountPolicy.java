6755bf6ec9329fffc7349ff674225ad367daa898
yoodongan <dbehddks519@naver.com>
2022-02-09 11:30:27 +0900

    add : 정률할인정책 실행과 테스트.

---
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,17 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class RateDiscountPolicy {
+    private int discount  = 10; // 할인율 10%를 의미.
+
+    public int discount(Member member, int price){
+        if (member.getGrade() == Grade.VIP){
+            return price * discount / 100;
+        }
+        else {
+            return 0;
+        }
+    }
+}
---
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,25 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+    RateDiscountPolicy rateDiscountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP 는 10% 할인입니다.")
+    void vip_ok(){
+        //given
+        Member member = new Member(1L, "memberVIP", Grade.VIP);
+        //when
+        int discount = rateDiscountPolicy.discount(member, 10000);
+        //then
+        assertThat(discount).isEqualTo(1000);
+
+    }
+
+}
